bdd12d30a187764f5eac669a1501cd74c86a3c76
Deividas <129685841+DeividasJuska@users.noreply.github.com>
2023-05-01 16:02:11 +0300

    Updates

---
+++ precache-manifest.d8cdd85671503f271f3f6ccba4a8470f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "909559f74abd35ddd117",
+    "url": "/robotest/static/css/main.e9c3eea4.chunk.css"
+  },
+  {
+    "revision": "909559f74abd35ddd117",
+    "url": "/robotest/static/js/main.909559f7.chunk.js"
+  },
+  {
+    "revision": "9594a6bb494a9d9ba425",
+    "url": "/robotest/static/js/1.9594a6bb.chunk.js"
+  },
+  {
+    "revision": "f522977c2d7ec0a02e4d",
+    "url": "/robotest/static/css/2.97efe846.chunk.css"
+  },
+  {
+    "revision": "f522977c2d7ec0a02e4d",
+    "url": "/robotest/static/js/2.f522977c.chunk.js"
+  },
+  {
+    "revision": "cc7781df345f0306692f",
+    "url": "/robotest/static/js/runtime~main.cc7781df.js"
+  },
+  {
+    "revision": "d358c0341ca4d6d5a83b62099e661e7f",
+    "url": "/robotest/static/media/PopArt-Regular.d358c034.ttf"
+  },
+  {
+    "revision": "454aa19e61e1edffc3a00406beb307f9",
+    "url": "/robotest/index.html"
+  }
+];
